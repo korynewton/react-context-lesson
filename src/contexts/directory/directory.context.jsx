a4245e824ab4643f87c575d006eb76114ad14222
Kory Newton <korybnewton@gmail.com>
2019-11-25 15:25:31 -0800

    convert remaining components to utilize context, redux completely removed

---
+++ src/contexts/directory/directory.context.jsx
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+
+import DIRECTORY_DATA from './directory.data.js';
+
+const DirectoryContext = createContext(DIRECTORY_DATA);
+
+export default DirectoryContext;
